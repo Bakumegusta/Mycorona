419a2a2d930344a35728e7f4f1c2ceda18d0eef3
Bakumegusta <bakumegusta@gmail.com>
2020-04-14 21:15:55 +0530

    fixed error

--- main.js
+++ main.js
@@ -31,72 +31,28 @@ function getcorona() {
                 });
             });
             // table rendering
-            var tblHtml =
-                "<table class='table ' id='coronaTable'>";
-            tblHtml += "<thead class = 'table-dark'>";
             $.each(data.statewise, function(i, d) {
-                if (i == 0) {
-                    tblHtml += "<tr>";
-                    var key1 = [];
+                console.log(d);
+                var parent = document.querySelector('#parent');
+                var row = document.createElement('tr');
+                row.innerHTML = `
+                              <td>${d.state}</td>
+                              <td>${d.recovered}</td>
+                              <td>${d.deaths}</td>
+                              <td>${d.confirmed}</td>
+                              <td>${d.active}</td>
+                              </tr>
 
-                    $.each(d, function(key, value) {
-                        key1.push(key);
-                    });
-                    var key2 = key1.reverse();
-                    // console.log(key2);
-
-                    $.each(key1, function(value, key) {
-                        // console.log(key);
-                        if (key != "delta" && key != 'deltaconfirmed' && key != 'deltadeaths' && key != 'deltarecovered' && key != 'lastupdatedtime' && key != 'statecode') {
-                            tblHtml += "<th>";
-                            tblHtml += key;
-                            tblHtml += "</th>";
-                        }
-
-                    });
-                    tblHtml += "</thead>";
-                    tblHtml += "<tbody>";
-                    $.each(data.statewise, function(i, d) {
-                        tblHtml += "<tr>";
-                        var value1 = [];
-                        $.each(d, function(key, value) {
-                            value1.push(value);
-                        });
-                        var value2 = value1.reverse();
-                        // console.log(value1);
-                        // console.log(d);
-                        $.each(value1, function(key, value) {
-                            // console.log(key, value);
-                            // console.log(key);
-                            if (value == null) {
-                                value = " ";
-                            }
-                            if (value == "NA") {
-                                value = " - ";
-                            }
-                            if (key != 0 && key != 3 && key != 4 && key != 5 && key != 6) {
-                                tblHtml += '<td>';
-                                tblHtml += value;
-                                tblHtml += '</td>';
-                            }
-
-                        });
-
-                    });
-                    tblHtml += "</tbody>";
-                    tblHtml += "</tbody>";
-                    $("#corona-tab").html("");
-                    $("#corona-tab").append(tblHtml);
-                    // $('#coronaTable').DataTable({
-                    //     responsive: true,
-                    //     colReorder: true,
-                    //     scrollY: '70vh',
-                    //     scrollCollapse: true,
-                    //     paging: false,
-                    //     "ordering": false
-                    // });
-                }
-
+                `;
+                parent.append(row);
+                // $('#coronaTable').DataTable({
+                //     responsive: true,
+                //     colReorder: true,
+                //     scrollY: '70vh',
+                //     scrollCollapse: true,
+                //     paging: false,
+                //     "ordering": false
+                // });
             });
 
         });
